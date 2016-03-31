ab9aedd569d1eb54e5a5d4efb64c7561d6e9a895
root <root@fedora-1.netgear.com>
2016-03-31 13:01:13 -0500

    updated second_program.py

--- second_program.py
+++ second_program.py
@@ -1,6 +1,10 @@
 # import python 3 division functionality of true division (no trunking of result)
 from __future__ import division
-print 'Hello World'
-print 'Hello again!'
+# import the python 3 print function
+from __future__ import print_function
+# since now using print function now put strings inside of quotes
+print('Hello World')
+print('Hello again!')
 num=(5+4)/4
-print num
+print(num)
+
